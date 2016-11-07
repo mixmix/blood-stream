12a2819f758f7d393014175bda2fb8da81259c5a
mix irving <whimful@gmail.com>
2016-11-07 13:31:54 +1300

    name a mode of death

--- organs/heart.js
+++ organs/heart.js
@@ -14,7 +14,8 @@ function Heart ({ initialState }) {
 
   const sink = (source) => {
     const readMore = (err, blood) => {
-      if (err) return console.log('DONE', err)
+      if (err) return console.log('You died from', err)
+      if (blood.sugar <= 0) return console.log('You died: blood sugar bottomed out')
 
       atrium = blood     // pass the blood into the atrium
       source(null, readMore)
